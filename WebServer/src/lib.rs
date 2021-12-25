c64ff59e24a319a4fac4836e4f6e3d4e7d81fd67
JiangXuanke <15223370366@163.com>
2021-12-25 20:24:30 +0900

    cut point

--- WebServer/src/lib.rs
+++ WebServer/src/lib.rs
@@ -1,18 +1,35 @@
-use std::thread;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::mpsc;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     pub fn new(size: unsize) -> ThreadPool {
         assert!(size > 0);
-        let mut threads = Vec::with_capacity(size);
-        for _ in 0..size {
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
 
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
+
         ThreadPool {
-            threads
+            workers,
+            sender,
         }
+    }
+}
+
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+
     }
 }
