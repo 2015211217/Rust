55c9bfd6127fbe9b3f2a3426c7291307dd6a8468
JiangXuanke <15223370366@163.com>
2021-12-25 18:19:19 +0900

    adding html files

--- WebServer/src/main.rs
+++ WebServer/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,8 +17,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-
+    // let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+    
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
